74eb77fd5b2dd3ee56f1545df0d6f4075603179d
root <root@xdl-189-39.ly.com>
2019-02-20 17:35:55 +0800

    del centos windows

--- windows/scripts/install-cloud-init.bat
+++
@@ -1,2 +0,0 @@
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.tcwyun.com/patch/CloudbaseInitSetup_0_9_9_x64.msi', 'C:\CloudbaseInitSetup_0_9_9_x64.msi')" <NUL
-msiexec /i c:\CloudbaseInitSetup_0_9_9_x64.msi /qr
